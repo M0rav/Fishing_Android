e41f0297151377ae1ddbb2383f5d660d92e537f9
Morva <Moravcsikmark@gmail.cim>
2023-02-16 20:21:30 +0100

    menu

--- app/src/main/java/hu/home/fishing/MainActivity.java
+++ app/src/main/java/hu/home/fishing/MainActivity.java
@@ -1,14 +1,79 @@
 package hu.home.fishing;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle toggle;
+
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //super.oncreate és setContentView között kell ezeket beállítani
+        //teljesképernyő beállítása
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //alapértelmezett actionbar (Toolbar) elrejtése
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        //alapértelmezett sötétmód kikapcsolása
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        init();
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.nav_main_page:
+                    frameLayout.setVisibility(View.GONE);
+                    break;
+                case R.id.nav_profile:
+                    frameLayout.setVisibility(View.VISIBLE);
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragmentContainer, new ProfileFragment()).commit();
+                    break;
+            }
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        });
+    }
+
+    public void init(){
+        toolbar = findViewById(R.id.toolBar);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigationView);
+        frameLayout = findViewById(R.id.fragmentContainer);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout,
+                toolbar,R.string.nyitva, R.string.zarva);
+    }
+    @Override
+    public void onBackPressed(){
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
     }
 }
